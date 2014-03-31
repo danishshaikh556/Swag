a3d5fe7cdeefe04a381973ca3528606601a07969
Danish Shaikh <danishshaikh556@gmail.com>
2014-03-30 18:03:51 -0700

    RetrofitApiCallClass
    Added Retrofit Api  Calling class
    Will be soon making it to a singleton Pattern

--- Swag/app/src/main/java/com/prolific/swag/app/Books.java
+++ Swag/app/src/main/java/com/prolific/swag/app/Books.java
@@ -3,6 +3,7 @@ package com.prolific.swag.app;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,39 +14,44 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class Books extends ListActivity implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-       //Displaying the title of the action Bar as "Books"
+        //Displaying the title of the action Bar as "Books"
         android.app.ActionBar actionBar = getActionBar();
         actionBar.setTitle("Books");
 
-        // add a ListView to fill the entire screen of the
-        // ListActivity, and pass into it an ArrayAdapter
-        // that manages the array of list items.
-        // Can also pass it an android-created list item:
-        //   android.R.layout.simple_list_item_1
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_books, COLORS));
+        ServerCalls performGet = new ServerCalls();
+                    performGet.execute("","","");
 
-        // obtain the ListView that was created by setListAdapter()
-        ListView myList = getListView();
+  }
 
-        // allow us to filter the list with keypresses
-        myList.setTextFilterEnabled(true);
+    //Called by doInBackGround()
+   public void fillListView()
+   {
+       // add a ListView to fill the entire screen of the
+       // ListActivity, and pass into it an ArrayAdapter
+       // that manages the array of list items.
+       // Can also pass it an android-created list item:
+       //   android.R.layout.simple_list_item_1
 
-        // implement an onClick listener for when a user taps a color
-        myList.setOnItemClickListener(this);
+       setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_books, toPass));
+       // obtain the ListView that was created by setListAdapter()
+       ListView myList = getListView();
+       // allow us to filter the list with keypresses
+       myList.setTextFilterEnabled(true);
+      // implement an onClick listener for when a user taps a color
+       myList.setOnItemClickListener(this);
 
-
-
-
-
-
-    }
-
+   }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -63,19 +69,16 @@ public class Books extends ListActivity implements AdapterView.OnItemClickListen
         int id = item.getItemId();
 
         switch(id){
-            case R.id.action_add :  LocationFound();
+            case R.id.action_add :  Intent i = new Intent(this, AddBooks.class);
+                                    startActivity(i);
                                     return true;
-            case R.id.action_seed:
-                Intent i = new Intent(this, Details.class);
-                startActivity(i);
-                return true;
+            case R.id.action_seed:  Intent j = new Intent(this, Details.class);
+                                    startActivity(j);
+                                    return true;
             default              :  return super.onOptionsItemSelected(item);
         }
     }
-    private void LocationFound() {
-        Intent i = new Intent(this, AddBooks.class);
-        startActivity(i);
-    }
+
 
 
 
@@ -93,16 +96,62 @@ public class Books extends ListActivity implements AdapterView.OnItemClickListen
         showMessage(item.getText());
     }
 
+     ///Server INteraction
+     //All tasks are async
 
-    // define a list of colors that the list will display
-    static final String[] COLORS = new String[] {
-            "Red",
-            "Orange",
-            "Yellow",
-            "Green",
-            "Blue",
-            "Indigo",
-            "Violet"
-    };
+    private class ServerCalls extends AsyncTask<String, String, String> {
+
+        public void  doGet() throws Exception, JSONException {
+
+            RetrofitAPICall getCall  = new RetrofitAPICall();
+                            AllBooks =  getCall.getFromServerAllBooks();
+
+            //Compatible for ListView
+            int stringArraySize = AllBooks.keySet().size();
+                         toPass = new String[stringArraySize];
+
+            //Create an itterator to itterate hasmap to display
+            //Required Data in the ListView
+             Iterator<String> itr = AllBooks.keySet().iterator();
+
+            int index =0;
+            while(itr.hasNext())
+            {
+                toPass[index++]=AllBooks.get(itr.next()).getId();
+            }
+
+        }
+
+
+        @Override
+        protected String doInBackground(String... params)  {
+            String toReturn ="";
+            try {
+                doGet();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return toReturn;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+                fillListView();
+          }
+        @Override
+        protected void onPreExecute() {}
+        @Override
+        protected void onProgressUpdate(String... text) {}
+
+
+    }
+
+
+
+
+
+    public HashMap<String,BookObject> AllBooks ;
+    public String[]          toPass            ;
+
 
 }
---
+++ Swag/app/src/main/java/com/prolific/swag/app/RetrofitAPICall.java
@@ -0,0 +1,168 @@
+package com.prolific.swag.app;
+
+import com.google.gson.JsonElement;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+import retrofit.RequestInterceptor;
+import retrofit.RestAdapter;
+import retrofit.client.Response;
+import retrofit.http.DELETE;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+/**
+ * Created by Danish556 on 3/30/14.
+ */
+public class RetrofitAPICall{
+
+private static final String API_URL =    "http://interview-danish.herokuapp.com";
+
+
+
+//Interface for GETRequests
+interface GETInterface {
+    @GET("/{book_URL}/{book_no_URL}")
+    JsonElement getData(
+            @Path("book_URL")      String book_URL,
+            @Path("book_no_URL") String book_no_URL
+    );
+}
+
+
+//Interface for DeleteRequests
+interface DeleteInterface{
+    @DELETE("/{single_bookOr_all}/{book_num_url}")
+    Response deleteBook(
+            @Path("single_bookOr_all") String single_bookOr_all,
+            @Path("book_num_url")      String book_num_url
+    );
+}
+
+
+
+
+
+    //Use this method to get a single book Using Retrofit@GET API
+    public String getFromServerSingleBook(String bookId) throws Exception {
+
+        // Create a very simple REST adapter which points the Your Site API  endpoint.
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint(API_URL)
+                .build();
+
+        // Create an instance of our GETInterface API interface.
+        GETInterface toGet       = restAdapter.create(GETInterface.class);
+        // Fetch and print a list of the contributors to this library.
+        JsonElement contributors = toGet.getData("books",bookId);
+
+        //Parse Received Data
+        String a           = contributors.toString();
+
+        JSONObject da      = null;
+        da      = new JSONObject(a);
+        JSONObject Abook   = new JSONObject(da.get("book").toString());
+        String toReturn    = Abook.get("id").toString();
+
+        return toReturn;
+    }
+
+
+
+
+    //Gets all books available at server using Retrofit @GET API
+    public HashMap<String,BookObject> getFromServerAllBooks () throws Exception
+    {
+        //Add headers to spoof android as a browse
+        //Note this step is only specific to the particular server-
+        //http://interview-danish.herokuapp.com/books
+        //As it doest accept mobile connections
+        RequestInterceptor requestInterceptor = new RequestInterceptor() {
+            @Override
+            public void intercept(RequestFacade request) {
+                request.addHeader("Accept", "text/html,application/xhtml+xml");
+                request.addHeader("Accept-Language", "en-US");
+                request.addHeader("User-Agent", "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0");
+                request.addHeader("Accept-Encoding", "gzip, deflate");
+                request.addHeader("Host","interview-danish.herokuapp.com");
+                request.addHeader("DNT", "1");
+                request.addHeader("Connection", "Keep-Alive");
+            }
+        };
+
+        // Create a very simple REST adapter which points the Your Site API  endpoint.
+        RestAdapter restAdapter2 = new RestAdapter.Builder()
+                .setEndpoint(API_URL)
+                .setRequestInterceptor(requestInterceptor)
+                .build();
+
+        // Create an instance of our GETInterface API interface.
+        GETInterface toGet = restAdapter2.create(GETInterface.class);
+        // Fetch and print a list of the contributors to this library.
+        JsonElement contributors = toGet.getData("books","");
+
+        //Parse The received Data
+        String a =contributors.toString();
+
+        JSONObject booksObj           = new JSONObject(a);
+        JSONArray booksArray         = booksObj.getJSONArray("books");
+
+        //Putting Recevied Data into a BookObject and Putting it into HashMap
+        for(int i =0;i< booksArray.length();i++)
+        {
+
+            JSONObject temp       = new JSONObject(booksArray.get(i).toString());
+            String title          = temp.get("title").toString() ;
+            String author         = temp.get("author").toString();
+            String categories     = temp.get("categories").toString();
+            String lastCheckedOut = temp.get("lastCheckedOutBy").toString();
+            String publisher      = temp.get("publisher").toString();
+            String urls           = temp.get("url").toString();
+            String id             = temp.get("id").toString();
+
+            BookObject tempBook   = new BookObject(author,title,publisher,lastCheckedOut,categories,urls,id);
+            AllBooks.put(tempBook.getId(), tempBook);
+        }
+
+        return AllBooks;
+    }
+
+
+    //Use this method to get a single book Using Retrofit@GET API
+    public String deleteSingleBookFromServer(String bookToDelete) throws Exception {
+
+        // Create a very simple REST adapter which points the Your Site API  endpoint.
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint(API_URL)
+                .build();
+
+        // Create an instance of our DeleteInterface API interface.
+        DeleteInterface toDelete = restAdapter.create(DeleteInterface.class);
+        // Delete from Library.
+        Response contributors = toDelete.deleteBook("books", bookToDelete);
+
+        return contributors.toString();
+    }
+
+
+    //Use this method to get a single book Using Retrofit@GET API
+    public String deleteAllBooksFromServer() throws Exception {
+
+        // Create a very simple REST adapter which points the Your Site API  endpoint.
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint(API_URL)
+                .build();
+
+        // Create an instance of our DeleteInterface API interface.
+        DeleteInterface toDelete = restAdapter.create(DeleteInterface.class);
+        // Delete from Library.
+        Response contributors = toDelete.deleteBook("clean","");
+
+        return contributors.toString();
+    }
+
+    private HashMap<String,BookObject> AllBooks = new HashMap<String, BookObject>();
+}
